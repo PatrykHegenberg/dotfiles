9ba295457b919364a976dd00182566ee4f290bf6
Patryk Hegenberg <patryk-hegenberg@outlook.de>
2023-10-31 10:48:40 +0100

    changed LazyVim config

---
+++ dot_config/LazyVim/lua/plugins/other.lua
@@ -0,0 +1,10 @@
+return {
+  -- tmux vim
+  { "christoomey/vim-tmux-navigator" },
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+  { "edluffy/hologram.nvim" },
+  {
+    "willothy/wezterm.nvim",
+    config = true,
+  },
+}
